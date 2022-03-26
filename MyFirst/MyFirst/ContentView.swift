fc316ce6c7aea5298a795f731be724e901cf99bc
shun yamasaka <118syuunico@gmail.com>
2022-03-26 19:11:13 +0900

    iosプロジェクト作成

---
+++ MyFirst/MyFirst/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFirst
+//
+//  Created by 山坂駿 on 2022/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyFirst/MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by 山坂駿 on 2022/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
