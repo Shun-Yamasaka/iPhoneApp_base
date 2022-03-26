242826d35c899625cfa60b78cc0b4bef504343f0
shun yamasaka <118syuunico@gmail.com>
2022-03-27 01:00:45 +0900

    地図アプリ追加/楽器アプリ変更登録

---
+++ MyMap/MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by 山坂駿 on 2022/03/27.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
