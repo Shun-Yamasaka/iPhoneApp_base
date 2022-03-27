b75d13601e570c904913af7f3a6b8bd312a1eef5
shun yamasaka <118syuunico@gmail.com>
2022-03-27 14:11:35 +0900

    キーワード機能追加

--- MyMap/MyMap/ContentView.swift
+++ MyMap/MyMap/ContentView.swift
@@ -8,11 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    // 入力中の文字列を保持する状態変数
+    @State var inputText: String = ""
+    // 検索キーワードを保持する状態変数
+    @State var dispSearchKey: String = ""
+    
+    
     var body: some View {
         // 垂直にレイアウト（縦方向にレイアウト）
         VStack{
+            // テキストフィールド（文字入力）
+            TextField("キーワード", text: $inputText, prompt:Text("キーワードを入力してください"))
+                // 入力が完了された時
+                .onSubmit{
+                    // 入力が完了したので検索キーワードに設定する
+                    dispSearchKey = inputText
+                }
+                // 余白を追加
+                .padding()
+            
             // マップを表示
-            MapView(serchKey: "東京タワー")
+            MapView(serchKey: dispSearchKey)
         } // VStack End
     }
 }
